e45d3351da6a9b133b837a22aaacd82758a60a27
Marcel Stimberg <marcel.stimberg@sorbonne-universite.fr>
2023-10-25 15:42:58 +0200

    rough prototype

---
+++ script.js
@@ -0,0 +1,171 @@
+const simulators = ["ANNarchy", "Arbor", "Brian", "Genesis", "GeNN",
+                    "Nest", "Neuron", "TheVirtualBrain"];
+const bio_levels = ["Population Model", "Single-Compartment (Simple) Model",
+                    "Single-Compartment (Complex) Model", "Multi-Compartment Model"]
+const comp_levels = ["Single Machine", "GPU", "Cluster", "Supercomputer"];
+
+const colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff",
+                "#00ffaa", "#aa00a0", "#afaaff"];
+
+function parse_file(simulator) {
+    const directory = "data/";
+    const url = directory + simulator + ".json";
+    data = fetch(url);
+    return data;
+}
+
+
+function parse_files() {
+    return Promise.all(simulators.map(sim => parse_file(sim))).then(
+        function(sims) {
+                return Promise.all(sims.map(s => s.json()));
+        }).then(
+        function(data) {
+            descriptions = {};
+            for (let i=0; i < simulators.length; i++ ) {
+                description = {};
+                // JSON file is a list on the top level instead of a dict
+                data[i].forEach(function(d) {
+                    description[Object.keys(d)[0]] = Object.values(d)[0];
+                });
+                descriptions[simulators[i]] = description;
+            }
+            return descriptions;
+        });
+}
+
+function calc_rectangle(description) {
+    let starts_x = [];
+    let ends_x = [];
+    let starts_y = [];
+    let ends_y = [];
+
+    for (let i=0; i<bio_levels.length; i++) {
+        if (description["biological_level"].indexOf(bio_levels[i]) > -1) {
+            if (starts_x.length == 0 || ends_x[ends_x.length-1] < i + 1) {
+                starts_x.push(i + 1);
+            }
+         } else {
+            if (starts_x.length > ends_x.length) {
+                ends_x.push(i);
+            }
+        }
+    }
+    if (starts_x.length > ends_x.length) {
+        ends_x.push(bio_levels.length);
+    }
+    for (let i=0; i<comp_levels.length; i++) {
+        if (description["computing_scale"].indexOf(comp_levels[i]) > -1) {
+            if (starts_y.length == 0 ||
+                (ends_y[ends_y.length-1] < i + 1 && starts_y.length === ends_y.length)) {
+                starts_y.push(i + 1);
+            }
+         } else {
+            if (starts_y.length > ends_y.length) {
+                ends_y.push(i);
+            }
+        }
+    }
+    if (starts_y.length > ends_y.length) {
+        ends_y.push(comp_levels.length);
+    }
+    return {'starts_x': starts_x, 'ends_x': ends_x,
+            'starts_y': starts_y, 'ends_y': ends_y};
+}
+
+function draw_rectangle(sim_idx, coords, svg) {
+    for (let i = 0; i < coords.starts_x.length; i++) {
+        for (let j = 0; j < coords.starts_y.length; j++) {
+            rect = document.createElementNS('http://www.w3.org/2000/svg', "rect");
+            rect.setAttribute("x", (coords.starts_x[i] - 0.5)*50 + sim_idx);
+            rect.setAttribute("y", (coords.starts_y[j] - 0.5)*50 + sim_idx);
+            rect.setAttribute("width", (coords.ends_x[i] - coords.starts_x[i] + 1)*50);
+            rect.setAttribute("height", (coords.ends_y[j] - coords.starts_y[j] + 1)*50);
+            rect.setAttribute("rx", 10);
+            rect.setAttribute("ry", 10);
+            // rect.setAttribute("stroke", "black");
+            rect.setAttribute("stroke-width", "1");
+            rect.setAttribute("fill", "transparent");
+            rect.setAttribute("stroke-opacity", "0.75");
+            rect.setAttribute("stroke", colors[sim_idx])
+            svg.appendChild(rect);
+        }
+    }
+}
+
+function mark_buttons(x, y, coords) {
+    for (let i = 0; i < simulators.length; i++) {
+        sim_coords = coords[simulators[i]];
+        let fits = false;
+        for (let j = 0; j < sim_coords.starts_x.length; j++) {
+            if (x/50 >= sim_coords.starts_x[j]-0.5 && x/50 <= sim_coords.ends_x[j]+0.5 &&
+                y/50 >= sim_coords.starts_y[j]-0.5 && y/50 <= sim_coords.ends_y[j]+0.5) {
+                fits = true;
+                break;
+            }
+        }
+        sim_button = document.getElementById(simulators[i]);
+        if (fits) {
+            sim_button.disabled = false;
+        } else {
+            sim_button.disabled = true;
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    parse_files().then(function(descriptions) {
+        let buttons_div = document.getElementById("buttons");
+        let buttons = [];
+        for (let i = 0; i < simulators.length; i++) {
+            let button = document.createElement("button");
+            button.innerHTML = simulators[i];
+            button.id = simulators[i];
+            buttons_div.appendChild(button);
+        }
+        // create SVG graph
+        let graph_div = document.getElementById("graph");
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width", "800");
+        svg.setAttribute("height", "500");
+        svg.setAttribute("viewBox", "0 0 250 350");
+        console.log(descriptions);
+        all_coords = {};
+        simulators.forEach(function(simulator, idx) {
+
+            coords = calc_rectangle(descriptions[simulator]);
+            all_coords[simulator] = coords;
+            draw_rectangle(idx, coords, svg);
+        });
+        
+        for (let i = 0; i < bio_levels.length; i++) {
+            text = document.createElementNS('http://www.w3.org/2000/svg', "text");
+            text.setAttribute("x", (i + 0.5)*50);
+            text.setAttribute("y", 250 + i*20);
+            textNode= document.createTextNode(bio_levels[i]);
+            text.appendChild(textNode);
+            svg.appendChild(text);
+        }
+        
+        for (let i = 0; i < comp_levels.length; i++) {
+            text = document.createElementNS('http://www.w3.org/2000/svg', "text");
+            text.setAttribute("y", (i + 0.5)*50 + 25);
+            text.setAttribute("x", 250);
+            textNode= document.createTextNode(comp_levels[i]);
+            text.appendChild(textNode);
+            svg.appendChild(text);
+        }
+
+        graph_div.appendChild(svg);
+        // Trigger function when hovering over the svg element
+        svg.addEventListener("mousemove", function(e) {
+            let x = e.clientX;
+            let y = e.clientY;
+            pt = svg.createSVGPoint();
+            pt.x = x;
+            pt.y = y;
+            const svgP = pt.matrixTransform( svg.getScreenCTM().inverse() );
+            mark_buttons(svgP.x, svgP.y, all_coords);
+        });
+    });
+});
